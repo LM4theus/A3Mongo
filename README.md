ae2081f0aebf0c03325b8dc568b58a6cf3ffcee1
Lucas Matheus <lucas.lm18rocha@gmail.com>
2025-06-05 16:30:08 -0300

    Adicionando FrontEnd para hospedagem na VERCEL

--- README.md
+++
@@ -1,2 +0,0 @@
-# A3Mongo
-Trabalho A3 da UC Sistema Distribuídos e Mobile - Sistema IoT com Python usando WebSocket e FastApi
--- iot-monitoring/dashboard/pages/index.js
+++ iot-monitoring/dashboard/pages/index.js
@@ -1,10 +1,10 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 
 export default function Dashboard() {
   const [message, setMessage] = useState("");
 
   useEffect(() => {
-    const socket = new WebSocket("wss://sua-api.render.com/ws");
+    const socket = new WebSocket("wss://a3mongo.onrender.com/ws");
     socket.onmessage = (event) => setMessage(event.data);
   }, []);
 
